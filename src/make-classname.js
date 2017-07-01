8f44c9ae07ef8891b7571c38506d2f5fe75a5c9e
cchanna <cerisa.accounts@strangerelics.com>
2017-07-01 15:49:14 -0700

    Change "styled" to "resplendent"

--- src/make-classname.js
+++ src/make-classname.js
@@ -1,7 +1,7 @@
 import hash from './hash';
 
 const makeStyledClassName = function(string, index) {
-  return `styled-${hash(string)}-${index}`;
+  return `resplendent-${hash(string)}-${index}`;
 };
 
 export default makeStyledClassName;
--- src/make-filename.js
+++ src/make-filename.js
@@ -2,7 +2,7 @@ import path from 'path';
 import hash from './hash';
 
 const makeFileName = (src, pathName, ext) => {
-    return path.join(src, '.generated', 'styled-' + hash(pathName) + '.g' + ext);
+    return path.join(src, '.generated', 'resplendent-' + hash(pathName) + '.g' + ext);
 }
 
 export default makeFileName;
